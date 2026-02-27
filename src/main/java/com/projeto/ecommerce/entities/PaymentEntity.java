ee65d6d9a5590cab447363f51481aef00a32d14b
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-02-27 08:43:15 -0300

    first commit

---
+++ src/main/java/com/projeto/ecommerce/entities/PaymentEntity.java
@@ -0,0 +1,17 @@
+package com.projeto.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+public class PaymentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private Instant moment;
+
+    @OneToOne
+
+}
