1312ebd0a83bdbbd94c946e55ec24dd7acc8a5ab
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-04-10 11:03:44 -0300

    feat: added security

---
+++ src/main/java/com/projeto/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.projeto.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").permitAll()
+                        .requestMatchers("/product").hasRole("ADMIN")
+                        .requestMatchers("/product/update/").hasRole("ADMIN")
+                        .requestMatchers("/product/delete/").hasRole("ADMIN")
+                        .requestMatchers("").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
